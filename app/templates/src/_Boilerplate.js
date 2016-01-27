3db160ada3d15c565e248132ea329e2a5eeefd46
Maira Bello <maira.araujo@liferay.com>
2016-01-27 09:41:33 -0300

    Stops using "npm:" prefix
    Paths that are neither absolute nor relative will already be treated as relative to node_modules.

--- app/templates/src/_Boilerplate.js
+++ app/templates/src/_Boilerplate.js
@@ -2,7 +2,7 @@
 
 import <%= capitalizeName %>Base from './<%= capitalizeName %>.soy';<% } else if (superClass === 'Attribute') { %>
 
-import Attribute from 'npm:metal/src/attribute/Attribute';<% } %>
+import Attribute from 'metal/src/attribute/Attribute';<% } %>
 
 class <%= capitalizeName %><% if (superClass !== 'none') { %> extends <% if (superClass === 'Component') { %><%= capitalizeName %>Base<% } else { %>Attribute<% } %><% } %> {
 }
