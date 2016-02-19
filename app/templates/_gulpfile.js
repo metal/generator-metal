c546bcc516db890fee53e3ac5445e530c4b6e526
Maira Bello <maira.araujo@liferay.com>
2016-02-19 10:15:00 -0300

    Changes mainBuildJsTasks option to use tasks with "js" suffix
    Those will be faster since they won't run the "soy" task on watch when js files change.

--- app/templates/_gulpfile.js
+++ app/templates/_gulpfile.js
@@ -4,8 +4,8 @@ var metal = require('gulp-metal');
 
 metal.registerTasks({
 	bundleCssFileName: '<%= lowercaseName %>.css',
-	bundleFileName: '<%= lowercaseName %>.js',
-	mainBuildJsTasks: ['build:<%= buildFormat %>'],
+	bundleFileName: '<%= lowercaseName %>.js',<% if (buildFormat !== 'globals') { %>
+	mainBuildJsTasks: ['build:<%= buildFormat %>:js'],<% } %>
 	moduleName: '<%= repoName %>'<% if (isNodeModule) { %>,
 	testNodeSrc: [
 		'env/test/node.js',
