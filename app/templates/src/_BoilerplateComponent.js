5609626d233f90427d736d56a3bb52df4921bc20
Maira Bello <maira.araujo@liferay.com>
2016-05-30 15:24:50 -0300

    Fixes generation of component without soy or jsx

--- app/templates/src/_BoilerplateComponent.js
+++ app/templates/src/_BoilerplateComponent.js
@@ -3,14 +3,15 @@
 import templates from './<%= capitalizeName %>.soy';
 import Component from 'metal-component';
 import Soy from 'metal-soy';<% } else if (templateLanguage === 'JSX') { %>
-import JSXComponent from 'metal-jsx';<% } %>
+import JSXComponent from 'metal-jsx';<% } else { %>
+import Component from 'metal-component';<% } %>
 
 <% if (templateLanguage === 'JSX') { %>class <%= capitalizeName %> extends JSXComponent {
 	render() {
 		return <div>Hello World</div>;
 	}
 }<% } else { %>class <%= capitalizeName %> extends Component {
-}
+}<% } %><% if (templateLanguage === 'Soy') { %>
 Soy.register(<%= capitalizeName %>, templates);<% } %>
 
 export default <%= capitalizeName %>;
