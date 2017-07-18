032c520c740d34378f3cb781cea58388b89f581c
Fernando Souza <nandosouzafilho@gmail.com>
2017-07-18 14:12:38 -0300

    Fixes jQuery build format

--- app/templates/src/_BoilerplateComponent.js
+++ app/templates/src/_BoilerplateComponent.js
@@ -4,7 +4,8 @@ import templates from './<%= componentName %>.soy.js';
 import Component from 'metal-component';
 import Soy from 'metal-soy';<% } else if (templateLanguage === 'JSX') { %>
 import JSXComponent from 'metal-jsx';<% } else { %>
-import Component from 'metal-component';<% } %>
+import Component from 'metal-component';<% } %><% if (buildFormat === 'jquery') { %>
+import JQueryAdapter from 'metal-jquery-adapter';<% } %>
 
 import './<%= kebabCaseName %>.scss';
 
@@ -14,7 +15,8 @@ import './<%= kebabCaseName %>.scss';
 	}
 }<% } else { %>class <%= componentName %> extends Component {
 }<% } %><% if (templateLanguage === 'Soy') { %>
-Soy.register(<%= componentName %>, templates);<% } %>
+Soy.register(<%= componentName %>, templates);<% } %><% if (buildFormat === 'jquery') { %>
+JQueryAdapter.register('<%= componentName %>', <%= componentName %>);<% } %>
 
 export { <%= componentName %> };
 export default <%= componentName %>;
